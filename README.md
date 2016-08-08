ad068b91392d65a7a96f4a1fd124aca5a77f046c
Nate Venn <natevenn@gmail.com>
2016-08-08 16:30:35 -0600

    initialize webpack setup and create readme (#1)
    * initialize webpack setup and create readme
    
    1. run npm init
    2. install react react-dom babel-loader babel-core babel-preset-es2015 and
    babel-preset-react
    3. create app/component/Main.js
    4. create app/App.js
    5. create public/index.html
    6. create webpack.config.js and configure webpack
    7. add script start to run server
    8. add script build to build webpack
    9. update readme
    
    * add dependencies to package.json
    
    Change license to MIT
    add private: true in package.json
    update readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # reactjsdenver.com
+
+Clone the repo:
+
+```
+$ git clone git@github.com:reactjsdenver/reactjsdenver.com.git
+```
+
+Install the dependencies:
+
+```
+$ npm install
+```
+
+Invoke webpack:
+
+```
+$ npm build
+```
+
+Start the dev server:
+
+```
+$ npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.export = {
+  entry: './app/App.js',
+  output: {
+    filename: 'public/bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
