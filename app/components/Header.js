9d048e8778927165dc946fd51f9d81f67471f9d8
Nathan Venn <natevenn@gmail.com>
2016-09-24 12:55:27 -0600

    import react logo for header

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,7 +6,9 @@ export default class Header extends Component{
     return(
       <nav className="ui header">
         <div className="react-denver-logo">
-        <h2>Denver ReactJS</h2>
+          <h2 className="logo-text-denver">Denver</h2>
+            <img className='react-logo' src="/style/images/react-logo.png" alt="react logo" />
+            <h2 className="logo-text-js">JS</h2>
         </div>
       </nav>
     );
