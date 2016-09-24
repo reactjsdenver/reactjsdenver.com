6e35889ba367cefb005ef550514c520ed32a4f9f
Nathan Venn <natevenn@gmail.com>
2016-09-24 10:52:09 -0600

    add google fonts

---
+++ app/components/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component{
+
+  render(){
+    return(
+      <nav className="ui header">
+        <div className="react-denver-logo">
+        <h2>Denver ReactJS</h2>
+        </div>
+      </nav>
+    );
+  }
+}
