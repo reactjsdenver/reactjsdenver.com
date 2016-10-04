2b662e87d84b9d954244544c15d9a5025d0973f1
Nate Venn <natevenn@gmail.com>
2016-10-04 17:54:23 -0600

    Feature/style logo (#3)
    * add google fonts
    
    * import react logo for header
    
    * resize logo and add white background to Denver text
    
    * remove white space from above header

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,7 +4,13 @@ export default class Header extends Component{
 
   render(){
     return(
-      <div className ="ui header"> Denver JS  </div>
+      <nav className="ui header">
+        <div className="react-denver-logo">
+          <h2 className="logo-text-denver">Denver</h2>
+            <img className='react-logo' src="/style/images/react-logo.png" alt="react logo" />
+            <h2 className="logo-text-js">JS</h2>
+        </div>
+      </nav>
     );
   }
 }
