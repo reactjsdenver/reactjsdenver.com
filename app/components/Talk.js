02f209f30b66f824aa3d140e9bc131328aa74f50
Regina Peyfuss <rpeyfuss@gmail.com>
2016-10-04 17:52:00 -0600

    add two styles for talk commonent in css; add button to submit talks

--- app/components/Talk.js
+++ app/components/Talk.js
@@ -1,17 +1,23 @@
+
+
 import React, {Component} from 'react'
 
 export default class Talk extends Component{
 
   render(){
     return(
-      <div className="ui grid">
+      <div className="talk-card">
+        <div className="ui grid">
 
-            <h2 className="center aligned text-color-white"> The Talks</h2>
+        <h2 className="center aligned text-color-white"> The Talks</h2>
 
-        <div className="row talk-background">
-          <div className="eight wide column">
+        <div className="row talk-background divided" >
+          <div className="eight wide column ">
             <h3>Free Slot</h3>
             <h4>This slot could be yours</h4>
+              <button className="talk-center ui basic button">
+                  Submit A Talk
+              </button>
             <h5>Do you need a talk idea?
               <br />We got you covered,
                 <a href="www.google.com">Click Me!</a>
@@ -20,6 +26,9 @@ export default class Talk extends Component{
           <div className="eight wide column">
             <h3>Free Slot</h3>
             <h4>This slot could be yours</h4>
+              <button className="ui basic button">
+                  Submit A Talk
+              </button>
             <h5>Do you need a talk idea?
               <br />We got you covered,
                 <a href="www.google.com">Click Me!</a>
@@ -27,7 +36,7 @@ export default class Talk extends Component{
 
           </div>
         </div>
-
+      </div>
       </div>
     );
   }
