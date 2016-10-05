5f2787258b5e170663b924b79ca90baa25897cad
Regina <rpeyfuss@gmail.com>
2016-10-04 18:31:11 -0600

    Feature/talk component (#4)
    * Add talk component and style
    
    * center text in all divs
    
    * add main-app class to css and to div in main.js

--- app/components/Talk.js
+++ app/components/Talk.js
@@ -1,25 +1,34 @@
+
+
 import React, {Component} from 'react'
 
 export default class Talk extends Component{
 
   render(){
     return(
-      <div className="ui grid">
+      <div className="talk-card">
+        <div className="ui grid center aligned">
 
-            <h2 className="center aligned text-color-white"> The Talks</h2>
+        <h2 className="text-color-white"> The Talks</h2>
 
-        <div className="row talk-background">
-          <div className="eight wide column">
+        <div className="row talk-background divided" >
+          <div className="center aligned eight wide column ">
             <h3>Free Slot</h3>
             <h4>This slot could be yours</h4>
+              <button className="ui basic button">
+                  Submit A Talk
+              </button>
             <h5>Do you need a talk idea?
               <br />We got you covered,
                 <a href="www.google.com">Click Me!</a>
             </h5>
           </div>
-          <div className="eight wide column">
+          <div className="center aligned eight wide column">
             <h3>Free Slot</h3>
             <h4>This slot could be yours</h4>
+              <button className="ui basic button">
+                  Submit A Talk
+              </button>
             <h5>Do you need a talk idea?
               <br />We got you covered,
                 <a href="www.google.com">Click Me!</a>
@@ -27,7 +36,7 @@ export default class Talk extends Component{
 
           </div>
         </div>
-
+      </div>
       </div>
     );
   }
