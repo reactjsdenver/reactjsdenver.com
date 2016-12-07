4f36dc43140bd7804a89a41eeb85fcc15526f3e9
Nathan Venn <natevenn@gmail.com>
2016-12-07 16:21:16 -0700

    change class declaration in main object

--- app/components/Meetup.js
+++ app/components/Meetup.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class Meetup extends React.Component {
+export default class Meetup extends Component {
   render() {
     return (
       <div className="meetup-main">
-        <h1 className="text typing one">Denver's monthly</h1> <br/>
+        <h1 className="text typing one">Denver's monthly</h1><br/>
         <h1 className="text typing two">React meetup</h1>
         <h2 className="text typing three">Next meeting is on
           <span className="meetup-date"> {this.props.date}</span>
@@ -17,5 +17,3 @@ class Meetup extends React.Component {
     )
   }
 }
-
-export default Meetup;
